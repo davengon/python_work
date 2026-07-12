fc3b3d0211c432b7e3ad8ef65dae8e5e2cc629ab
David <davidgonzalez5910@gmail.com>
2026-07-12 17:01:24 -0500

    Ex 8.4

--- functions/Tshirt.py
+++ functions/Tshirt.py
@@ -1,7 +1,10 @@
-def make_shirt(size, text):
+def make_shirt(size = "L", text = "I love Python!"): #Default Values, if we don't provide arguments, the function can make use of the default values to fill up what's missing.
     print(f"Your new Tshirt has a size of {size}, and it says {text}" )
 
 make_shirt("S", "Hello World!") #Positional Arguments, Python will read them in order and assign them to the parameters in the order provided.
 make_shirt(text = "Hello Python!", size = "M") #Keyword Arguments, by providing the name of the argument, python knows to which parameter it belongs each piece of data.
+make_shirt()
+make_shirt("S", "Python Crash Course Student")
+
 
 
