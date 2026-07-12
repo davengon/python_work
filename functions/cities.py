64926adfed1408d201fc403be1173270668ea9cb
David <davidgonzalez5910@gmail.com>
2026-07-12 17:04:49 -0500

    Ex 8.5

---
+++ functions/cities.py
@@ -0,0 +1,6 @@
+def describe_city(city, country = "Colombia"):
+    print(f"{city.title()} is in {country.title()}")
+
+describe_city("Medellin")
+describe_city("Tokyo", "Japan")
+describe_city("Cartagena")
