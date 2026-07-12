872b6c60bb63a06ccfb3e87833c9de1fb48c0d62
David <davidgonzalez5910@gmail.com>
2026-07-12 18:17:56 -0500

    Ex 8.11

--- functions/archived_messages.py
+++ functions/archived_messages.py
@@ -14,5 +14,5 @@ def send_messages(messages):
 
 text_messages = ["Hi", "my name is David", "nice meeting you!"]
 show_messages(text_messages)
-print(send_messages(text_messages[:]))
+print(send_messages(text_messages[:])) #The : uses a copy of the list thus the original is not affected by the function and its original content remain the same
 print(text_messages)
