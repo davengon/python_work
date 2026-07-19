2d14467cd1018d9eb34f9c267b9c44c37ad01ba7
David <davidgonzalez5910@gmail.com>
2026-07-19 08:47:19 -0500

    Ex 9.1

---
+++ classes/restaurant.py
@@ -0,0 +1,17 @@
+class Restaurant:
+    
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"Our restaurant is called: {self.restaurant_name}, and we work with {self.cuisine_type} cuisine!")
+    
+    def open_restaurant(self):
+        print(f"The restaurant {self.restaurant_name} is open!")
+
+my_restaurant = Restaurant("Lime Ceviche", "Peruvian")
+print(my_restaurant.restaurant_name)
+print(my_restaurant.cuisine_type)
+my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
