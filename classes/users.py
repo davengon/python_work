aef44d78e2386aa04d186941d4b64589f5eabfe2
David <davidgonzalez5910@gmail.com>
2026-07-19 09:19:24 -0500

    Ex 9.5

--- classes/users.py
+++ classes/users.py
@@ -5,12 +5,19 @@ class User:
         self.last_name = last_name
         self.phone_number = phone_number
         self.id = id
+        self.login_attempts = 0
     
     def describe_user(self):
         print(f"The user's name is: {self.first_name + " " + self.last_name}, their phone number is {self.phone_number}, and their id is: {self.id}")
     
     def greet_user(self):
         print(f"Hello, {self.first_name}, welcome back!")
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+    
+    def reset_login_attempts(self):
+        self.login_attempts = 0
     
 first_user = User("David", "Gallego", "154654654", "0001")
 second_user = User("Santiago", "Gal", "464564654", "0002")
@@ -19,3 +26,9 @@ first_user.greet_user()
 first_user.describe_user()
 second_user.greet_user()
 second_user.describe_user()
+
+first_user.increment_login_attempts()
+first_user.increment_login_attempts()
+print(first_user.login_attempts)
+first_user.reset_login_attempts()
+print(first_user.login_attempts)
