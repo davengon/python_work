be20990847f313eae58f63e28fa04b52d9fcfeee
David <davidgonzalez5910@gmail.com>
2026-07-21 18:24:35 -0500

    Ex 9.6

--- classes/restaurant.py
+++ classes/restaurant.py
@@ -16,6 +16,18 @@ class Restaurant:
 
     def increment_number_served(self, new_customers):
         self.number_served += new_customers
+
+class IceCreamStand(Restaurant):
+    
+    def __init__(self, restaurant_name, cuisine_type):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ["Bubble Gum", "Pistaccio", "Vanilla", "Blackberry sorbet"]
+    
+    def display_menu(self):
+        print("These are our options: \n")
+        for flavor in self.flavors:
+            print(flavor)
+        
         
 
 my_restaurant = Restaurant("Lime Ceviche", "Peruvian")
@@ -38,4 +50,6 @@ print(f"The restaurant has served {restaurant.number_served} customers")
 restaurant.increment_number_served(55)
 print(f"The restaurant has served {restaurant.number_served} customers")
 
+ice_cream_shop = IceCreamStand("Gellato", "Deserts")
+ice_cream_shop.display_menu()
 
